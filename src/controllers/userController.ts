6b50ff276df093f6be221e3f841e0dd6a00f2028
Emir Salihovic <emir.home.11@gmail.com>
2024-07-18 13:16:49 +0200

    feat(authentication): login route
    users login, hasing the password and comparing it on login

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import User from '../models/userModel';
+import User, { IUser } from '../models/userModel';
 import asyncHandler from '../utils/asyncHandler';
 import createSendToken from '../utils/signToken';
 
@@ -16,4 +16,28 @@ export const signup = asyncHandler(async (req: Request, res: Response) => {
   });
 
   createSendToken(newUser, 201, req, res);
+});
+
+export const login = asyncHandler(async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  // 1) Check if username and password exist
+  if (!username || !password) {
+    res.status(400).json({
+      error: 'Please provide username and password.'
+    });
+    return;
+  }
+  // 2) Check if user exists && password is correct
+  const user = await User.findOne({ username }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    res.status(401).json({
+      error: 'Incorrect username or password'
+    });
+    return;
+  }
+
+  // 3) If everything ok, send token to client
+  createSendToken(user as IUser, 200, req, res);
 });
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,9 +1,14 @@
 import { Document, Schema, Model, model } from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 // Define an interface representing a document in MongoDB.
 export interface IUser extends Document {
   username: string;
   password: string;
+  correctPassword(
+    candidatePassword: string,
+    userPassword: string
+  ): Promise<boolean>;
 }
 
 // Create a Schema corresponding to the document interface.
@@ -20,6 +25,23 @@ const UserSchema: Schema<IUser> = new Schema<IUser>(
     versionKey: false
   }
 );
+
+UserSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  next();
+});
+
+UserSchema.methods.correctPassword = async function (
+  candidatePassword: string,
+  userPassword: string
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 // Create a Model.
 const User: Model<IUser> = model<IUser>('User', UserSchema);

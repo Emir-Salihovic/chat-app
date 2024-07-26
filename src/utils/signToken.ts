40c5715cdf292397a9901cc509457285e9eadbaf
Emir Salihovic <emir.home.11@gmail.com>
2024-07-26 02:31:29 +0200

    fix(cookies): same site problem

--- src/utils/signToken.ts
+++ src/utils/signToken.ts
@@ -30,7 +30,8 @@ const createSendToken = (
       expires: new Date(
         +Date.now() + +process.env.JWT_COOKIE_EXPIRES_IN! * 24 * 60 * 60 * 1000
       ),
-      httpOnly: true
+      httpOnly: true,
+      sameSite: 'none'
     });
   }
 
